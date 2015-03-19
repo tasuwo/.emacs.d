9b033d7c9cea3ddc72df641d816577e84e27e809
tasuku tozawa <kamuhata.you@gmail.com>
2015-03-19 11:37:28 +0900

    C/C++モードのインデント変更

--- inits/00-mode.el
+++ inits/00-mode.el
@@ -4,18 +4,23 @@
 (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
-
 ;; ファイル内容を標準入力で渡すのではなく、ファイル名を引数として渡すように設定
 (defun markdown-custom ()
   "markdown-mode-hook"
   (setq markdown-command-needs-filename t)
     )
-
 (add-hook 'markdown-mode-hook '(lambda() (markdown-custom)))
 (add-hook 'gfm-mode-hook
           '(lambda ()
              (markdown-custom)
              (setq global-linum-mode nil)
              (electric-indent-local-mode -1)))
+
+;; C/C++
+(defun my-c-c++-mode-init ()
+  (setq c-basic-offset 4)
+)
+(add-hook 'c-mode-hook 'my-c-c++-mode-init)
+(add-hook 'c++-mode-hook 'my-c-c++-mode-init)
 
 ;;; 00-mode.el ends here
--- inits/10-auto-complete.el
+++ inits/10-auto-complete.el
@@ -1,3 +1,8 @@
+
+;;; Code:
 (require 'auto-complete)
 (require 'auto-complete-config)    ; 必須ではないですが一応
 (global-auto-complete-mode t)
+(ac-config-default)
+
+;;; 10-auto-complete.el ends here
