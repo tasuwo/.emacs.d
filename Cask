9e21917b6ad0bd31560e9897802609dd0e914ee2
tasuku tozawa <kamuhata.you@gmail.com>
2015-06-22 13:29:54 +0900

    add php-mode

--- Cask
+++ Cask
@@ -1,5 +1,6 @@
 (source gnu)
 (source melpa)
+(source marmalade)
 
 (depends-on "js-doc")
 (depends-on "ac-slime" :git "https://github.com/purcell/ac-slime")
@@ -17,6 +18,7 @@
 (depends-on "popup")
 (depends-on "popwin" :git "https://github.com/m2ym/popwin-el")
 (depends-on "rainbow-delimiters")
-(depends-on "slime" :git "https://github.com/slime/slime")
+(depends-on "slime" :git "https://github.com/slime/slime.git")
 (depends-on "smartparens")
 (depends-on "smooth-scroll")
+(depends-on "php-mode" :git "https://github.com/ejmr/php-mode")
--- inits/00-mode.el
+++ inits/00-mode.el
@@ -49,4 +49,19 @@
 (slime-setup '(slime-repl slime-fancy slime-banner slime-indentation))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; php
+(require 'php-mode)
+(add-hook 'php-mode-hook
+          (lambda ()
+            (require 'php-completion)
+            (php-completion-mode t)
+            (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
+            (make-local-variable 'ac-sources)
+            (setq ac-sources '(
+                               ac-source-words-in-same-mode-buffers
+                               ac-source-php-completion
+                               ac-source-filename
+                                                              ))))
+
 ;;; 00-mode.el ends here
