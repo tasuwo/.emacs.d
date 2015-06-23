fa068cd86b357cdf3574bbe2fc340373f21df6d6
tasuku tozawa <kamuhata.you@gmail.com>
2015-06-23 16:32:06 +0900

    remove slime.

--- inits/00-mode.el
+++ inits/00-mode.el
@@ -41,12 +41,12 @@
 ;;; slime
 ;; デフォルトの Common Lisp 処理系
 ;(setq inferior-lisp-program "ccl")     ; Clozure CL
-(setq inferior-lisp-program "clisp")   ; CLISP
+;(setq inferior-lisp-program "clisp")   ; CLISP
 ;; SLIMEのロード
-(require 'slime)
+;(require 'slime)
 ;; SLIMEからの入力をUTF-8に設定
-(setq slime-net-coding-system 'utf-8-unix)
-(slime-setup '(slime-repl slime-fancy slime-banner slime-indentation))
+;(setq slime-net-coding-system 'utf-8-unix)
+;(slime-setup '(slime-repl slime-fancy slime-banner slime-indentation))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
