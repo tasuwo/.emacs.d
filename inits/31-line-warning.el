c3a322b7a320f3955da33fd6962024c5f86eb515
tasuwo <kamuhata.you@gmail.com>
2015-07-29 08:51:19 +0900

    行ごとの文字数について警告を表示する

---
+++ inits/31-line-warning.el
@@ -0,0 +1,28 @@
+
+;;; Code:
+
+;;; C系統,Pythonにて1行80文字を超えるとハイライト
+(add-hook 'c-mode-hook
+  (lambda ()
+    (font-lock-add-keywords nil
+      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+(add-hook 'c++-mode-hook
+  (lambda ()
+    (font-lock-add-keywords nil
+      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+(add-hook 'python-mode-hook
+  (lambda ()
+    (font-lock-add-keywords nil
+      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+(add-hook 'matlab-mode-hook
+  (lambda ()
+    (font-lock-add-keywords nil
+      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+
+;;; Javaで1行100文字を超えるとハイライト
+(add-hook 'java-mode-hook
+  (lambda ()
+    (font-lock-add-keywords nil
+      '(("^[^\n]\\{100\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
+
+;;; 31-line-warning.el ends here
