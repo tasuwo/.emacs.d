6212661b991579ac66e1dd852590fbe726657dd8
tasuwo <kamuhata.you@gmail.com>
2015-07-28 17:34:39 +0900

    flycheckの設定にコメント追加

--- inits/20-flycheck.el
+++ inits/20-flycheck.el
@@ -1,5 +1,11 @@
+
+;;; Code:
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;; flycheck-pos-chip の設定
 (eval-after-load 'flycheck
   '(custom-set-variables
        '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+;;; 20-flycheck.el ends here
