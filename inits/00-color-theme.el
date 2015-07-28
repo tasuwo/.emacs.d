4e94211e387c4a7d0f0038fc8a3c3443774c8f51
tasuwo <kamuhata.you@gmail.com>
2015-07-28 23:13:12 +0900

    テーマをかえた

--- inits/00-color-theme.el
+++ inits/00-color-theme.el
@@ -1,1 +1,9 @@
-(require 'darcula-theme)
+
+;;; Code:
+
+;; http://emacsthemes.caisah.info/
+;;(require 'darcula-theme)
+;; http://aoe-tk.hatenablog.com/entry/20130210/1360506829
+(load-theme 'misterioso t)
+
+;;; 00-color-theme.el ends here
