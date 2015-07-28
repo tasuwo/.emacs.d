1277c8a52435af647e0d7fd25601d74780b3cd88
tasuwo <kamuhata.you@gmail.com>
2015-07-28 17:10:13 +0900

    matlabモードに向けて設定変更．Flycheckについてちょっと調べる．

--- inits/20-edit-mode-matlab.el
+++ inits/20-edit-mode-matlab.el
@@ -2,22 +2,28 @@
 ;;; Code:
 
 (autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
-
+(autoload 'matlab-shell "matlab" "Interactive MATLAB mode." t)
 (setq auto-mode-alist (cons '("\\.m$" . matlab-mode) auto-mode-alist))
-(add-hook 'matlab-mode-hook
-          '(lambda ()
-             (set-buffer-file-coding-system 'sjis-dos))) ;; M-ファイルはシフトJISで開く
 
-(setq matlab-shell-command "/Applications/MATLAB_R2012a_Student.app/bin/matlab"  ;; bashにパス通しても上手くいかなかったので
-      matlab-indent-level 2  ;; ここら辺は好みでどうぞ
+;; auto-complete-modeの自動起動
+(add-to-list 'ac-modes 'matlab-mode)
+
+;;(add-hook 'matlab-mode-hook
+;;          '(lambda ()
+;;             (set-buffer-file-coding-system 'sjis-dos))) ;; M-ファイルはシフトJISで開く
+
+(setq matlab-shell-command "/Applications/MATLAB_R2014a.app/bin/matlab"  ;; bashにパス通しても上手くいかなかったので
+      matlab-indent-level 4  ;; ここら辺は好みでどうぞ
       matlab-indent-function-body nil
       matlab-highlight-cross-function-variables t
       matlab-return-add-semicolon t
       matlab-show-mlint-warnings t
-      mlint-programs '("/Applications/MATLAB_R2012a_Student.app/bin/maci64/mlint")
+      mlint-programs '("/Applications/MATLAB_R2014a.app/bin/maci64/mlint")
       matlab-mode-install-path (list (expand-file-name "~/.emacs.d/elisp/matlab/"))
       )
 
+;;(eval-after-load 'flycheck
+;;  '(require 'flycheck-matlab-mlint))
 (autoload 'mlint-minor-mode "mlint" nil t)
 (add-hook 'matlab-mode-hook (lambda () (mlint-minor-mode 1)))
 (add-hook 'matlab-shell-mode-hook 'ansi-color-for-comint-mode-on)
