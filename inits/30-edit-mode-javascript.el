2222f2986fcfa38d07e54ae281174fbda89aa41c
tasuwo <kamuhata.you@gmail.com>
2016-02-03 12:11:18 +0900

    Modify indent level at javascript-mode

--- inits/30-edit-mode-javascript.el
+++ inits/30-edit-mode-javascript.el
@@ -10,7 +10,7 @@
 
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\.js$" . js2-mode))
-
+(setq js-indent-level 2)
 (add-hook 'js2-mode-hook
           (lambda ()
             (tern-mode t)))
