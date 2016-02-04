a243befda9f70282bea8327f149d1d3066f8145d
tasuwo <kamuhata.you@gmail.com>
2016-02-05 08:42:50 +0900

    Add setting of font to basic.el

--- inits/00-basic.el
+++ inits/00-basic.el
@@ -1,6 +1,17 @@
 
 ;;; Code:
 
+(setq locale-coding-system 'utf-8)
+
+;; font
+(create-fontset-from-ascii-font "Meslo LG S for Powerline:style=RegularForPowerline" nil "powerline")
+(set-fontset-font "fontset-powerline"
+                  'unicode
+                  (font-spec :family "Meslo LG S for Powerline" :size 16)
+                  nil
+                  'append)
+(add-to-list 'default-frame-alist '(font . "fontset-powerline"))
+
 ;; 文字コード
 (set-language-environment "Japanese")
 (let ((ws window-system))
