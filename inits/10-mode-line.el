ec10128ac8be86c4e0e611a7ced5b1892a07c754
tasuwo <kamuhata.you@gmail.com>
2016-03-19 11:36:19 +0900

    Clean mode line

--- inits/10-mode-line.el
+++ inits/10-mode-line.el
@@ -7,43 +7,62 @@
 (setq sml/theme 'dark)
 (sml/setup)
 
+(line-number-mode t)
+(column-number-mode t)
 (setq-default mode-line-format
-              '("-"
+              '(" "
                 mode-line-mule-info
                 mode-line-modified
                 mode-line-frame-identification
                 mode-line-buffer-identification
                 " "
                 global-mode-string
-                " %[("
+                "%[("
                 mode-name
                 mode-line-process
                 minor-mode-alist
-                "%n" ")%]-"
-                (which-func-mode ("" which-func-format "-"))
-                (line-number-mode "L%l-")
-                (column-number-mode "C%c-")
-                (-3 . "%p")
-                "-%-")
-              )
+                ")%]" " " "%[("
+                (line-number-mode "L%l")
+                ("" (:eval (format "/%s" (line-number-at-pos (point-max)))))
+                (column-number-mode " C%c")
+                "%])"))
 
-;; http://syohex.hatenablog.com/entry/20130131/1359646452
 (defvar mode-line-cleaner-alist
-  '( ;; For minor-mode, first char is 'space'
-    (yas-minor-mode . " Ys")
-    (paredit-mode . " Pe")
-    (eldoc-mode . "")
-    (abbrev-mode . "")
-    (undo-tree-mode . " Ut")
-    (elisp-slime-nav-mode . " EN")
-    (helm-gtags-mode . " HG")
-    (flymake-mode . " Fm")
-    ;; Major modes
-    (lisp-interaction-mode . "Li")
-    (python-mode . "Py")
-    (ruby-mode   . "Rb")
-    (emacs-lisp-mode . "El")
-    (markdown-mode . "Md")))
+  '(
+    ;; minor mode
+    (auto-complete-mode .     " AC")
+    (flycheck-mode .          " FC")
+    (yas-minor-mode .         " YS")
+    (paredit-mode .           "")
+    (eldoc-mode .             "")
+    (smartparens-mode .       " SP")
+    (whitespace-mode .        "")
+    (global-whitespace-mode . "")
+    (undo-tree-mode .         "")
+    (elisp-slime-nav-mode .   "")
+    (helm-gtags-mode .        "")
+    (helm-mode .              " HE")
+    (git-gutter+-mode .       " GT")
+    (ace-isearch-mode .       "")
+    (shell-mode .             " AR")
+    (auto-revert-mode .       "")
+    (helm-migemo-mode .       " HM")
+    (lisp-interaction-mode .  "")
+    ;; major mode
+    (python-mode     . "Python")
+    (ruby-mode       . "Ruby")
+    (emacs-lisp-mode . "EmacsLisp")
+    (markdown-mode   . "Markdown")
+    (c-mode          . "Clang")
+    (c++-mode        . "C++")
+    (swift-mode      . "Swift")
+    (web-mode        . "Web")
+    (javascript-mode . "JavaScript")
+    (slim-mode       . "Slim")
+    (coffee-mode     . "CoffeeScript")
+    (matlab-mode     . "Matlab")
+    (php-mode        . "PHP")
+    (typescript-mode . "TypeScript")))
 
 (defun clean-mode-line ()
   (interactive)
