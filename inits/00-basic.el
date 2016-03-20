d104fad1af20e63124f8937c9151a9be6fdfb961
tasuwo <kamuhata.you@gmail.com>
2016-03-20 13:30:20 +0900

    Add setting of font

--- inits/00-basic.el
+++ inits/00-basic.el
@@ -12,6 +12,28 @@
 ;;                   nil
 ;;                   'append)
 ;; (add-to-list 'default-frame-alist '(font . "fontset-powerline"))
+(cond (darwin-p
+  (global-set-key [s-mouse-1] 'browse-url-at-mouse)
+  (let* ((size 14)
+         (jpfont "Hiragino Maru Gothic ProN")
+         (asciifont "Monaco")
+         (h (* size 10)))
+    (set-face-attribute 'default nil :family asciifont :height h)
+    (set-fontset-font t 'katakana-jisx0201 jpfont)
+    (set-fontset-font t 'japanese-jisx0208 jpfont)
+    (set-fontset-font t 'japanese-jisx0212 jpfont)
+    (set-fontset-font t 'japanese-jisx0213-1 jpfont)
+    (set-fontset-font t 'japanese-jisx0213-2 jpfont)
+    (set-fontset-font t '(#x0080 . #x024F) asciifont))
+  (setq face-font-rescale-alist
+        '(("^-apple-hiragino.*" . 1.2)
+          (".*-Hiragino Maru Gothic ProN-.*" . 1.2)
+          (".*osaka-bold.*" . 1.2)
+          (".*osaka-medium.*" . 1.2)
+          (".*courier-bold-.*-mac-roman" . 1.0)
+          (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+          (".*monaco-bold-.*-mac-roman" . 0.9)
+          ("-cdac$" . 1.3)))))
 
 ;; 文字コード
 (set-language-environment "Japanese")
@@ -26,9 +48,9 @@
 (set-clipboard-coding-system 'utf-8)
 
 ;; フォントロック
-(global-font-lock-mode 1)
-(setq font-lock-support-mode 'jit-lock-mode)
-(setq font-lock-maximum-decoration t)
+;; (global-font-lock-mode 1)
+;; (setq font-lock-support-mode 'jit-lock-mode)
+;; (setq font-lock-maximum-decoration t)
 
 ;; 行番号表示
 (global-linum-mode t)
