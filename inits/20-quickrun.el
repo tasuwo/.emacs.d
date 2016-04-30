82b2e15634f20d22d7e230c5bc27fa9af7d2c309
tasuwo <kamuhata.you@gmail.com>
2016-05-01 08:56:00 +0900

    Compile coffee script by quickrun

--- inits/20-quickrun.el
+++ inits/20-quickrun.el
@@ -3,4 +3,11 @@
 
 (use-package quickrun)
 
+(quickrun-add-command "coffee-compile"
+                      '((:command . "coffee")
+                        (:exec . "%c --compile --print %s")
+                        (:outputter . (lambda () (javascript-generic-mode))))
+                      :default "coffee")
+(require 'generic-x)
+
 ;;; 20-quickrun.el ends here
