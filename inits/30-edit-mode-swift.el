41f1263bca4ab3a771c90a933203cff205fffda3
tasuwo <kamuhata.you@gmail.com>
2016-05-01 09:00:33 +0900

    Modify swift-mode

--- inits/30-edit-mode-swift.el
+++ inits/30-edit-mode-swift.el
@@ -1,21 +1,19 @@
 
 ;;; Code:
 
-(setq flycheck-swift-sdk-path
-      (replace-regexp-in-string
-       "\n+$" "" (shell-command-to-string
-                  "xcrun --show-sdk-path --sdk macosx")))
-
 (require 'swift-mode)
 (require 'auto-complete-swift)
 
-(add-to-list 'flycheck-checkers 'swift)
 (add-hook 'swift-mode-hook
-  '(lambda()
-    (local-set-key "\C-c\C-c" 'quickrun)
-    (local-set-key "\C-c\C-a" 'quickrun-with-arg)
-    (add-to-list 'flycheck-checkers 'swift)
-    (push 'ac-source-swift-complete ac-sources)
+          '(lambda()
+             (setq flycheck-swift-sdk-path
+                   (replace-regexp-in-string
+                    "\n+$" "" (shell-command-to-string
+                               "xcrun --show-sdk-path --sdk macosx")))
+             (local-set-key "\C-c\C-c" 'quickrun)
+             (local-set-key "\C-c\C-a" 'quickrun-with-arg)
+             (add-to-list 'flycheck-checkers 'swift)
+             (push 'ac-source-swift-complete ac-sources)
   )
 )
 
