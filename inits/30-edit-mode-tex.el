895f0d1d7eab280ed97ea3e123e9f5c164b3fe4e
tasuwo <kamuhata.you@gmail.com>
2016-05-15 23:02:37 +0900

    Update tex config

--- inits/30-edit-mode-tex.el
+++ inits/30-edit-mode-tex.el
@@ -6,12 +6,16 @@
   (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
 
+(setq tex-command "platex")
+(setq bibtex-command "pbibtex")
+(setq YaTeX-nervous nil)
+
 (setq tex-command "platex2pdf") ;; 自作したコマンドを
 (cond
-  ((eq system-type 'gnu/linux) ;; GNU/Linux なら
-    (setq dvi2-command "evince")) ;; evince で PDF を閲覧
+  ((eq system-type 'gnu/linux)
+    (setq dvi2-command "evince"))
   ((eq system-type 'darwin) ;; Mac なら
-    (setq dvi2-command "open -a Preview"))) ;; プレビューで
+    (setq dvi2-command "open -a Preview")))
 (add-hook 'yatex-mode-hook '(lambda () (setq auto-fill-function nil)))
 
 ;;; 30-edit-mode-tex.el ends here
