431e849130715e921c7cb22003aeb6e415c67640
tasuwo <kamuhata.you@gmail.com>
2016-12-13 17:21:21 +0900

    update

--- inits/20-git-gutter-plus.el
+++ inits/20-git-gutter-plus.el
@@ -4,23 +4,24 @@
 
 ;;; Code:
 
-(use-package git-gutter+
-  :config
-  (setq git-gutter+-modified-sign "!")
-  (setq git-gutter+-added-sign "+")
-  (setq git-gutter+-deleted-sign "-")
-
-  (set-face-foreground 'git-gutter+-modified "yellow")
-  (set-face-foreground 'git-gutter+-added    "green")
-  (set-face-foreground 'git-gutter+-deleted  "red")
-
-  (global-git-gutter+-mode)
-
-  ;; fringe に git 情報を載せる
-  (use-package git-gutter-fringe+
+(when (require 'git-gutter+ nil 'noerror)
+  (use-package git-gutter+
     :config
-    (set-face-foreground 'git-gutter-fr+-modified "yellow")
-    (set-face-foreground 'git-gutter-fr+-added    "green")
-    (set-face-foreground 'git-gutter-fr+-deleted  "red")))
+    (setq git-gutter+-modified-sign "!")
+    (setq git-gutter+-added-sign "+")
+    (setq git-gutter+-deleted-sign "-")
+
+    (set-face-foreground 'git-gutter+-modified "yellow")
+    (set-face-foreground 'git-gutter+-added    "green")
+    (set-face-foreground 'git-gutter+-deleted  "red")
+
+    (global-git-gutter+-mode)
+
+    ;; fringe に git 情報を載せる
+    (use-package git-gutter-fringe+
+      :config
+      (set-face-foreground 'git-gutter-fr+-modified "yellow")
+      (set-face-foreground 'git-gutter-fr+-added    "green")
+      (set-face-foreground 'git-gutter-fr+-deleted  "red"))))
 
 ;;; 20-git-gutter-plus.el ends here
