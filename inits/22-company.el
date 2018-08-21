af5e27cb122fc8365d7730994093654a25b1cecd
tasuwo <kamuhata.you@gmail.com>
2018-08-21 23:53:28 +0900

    update

--- inits/22-company.el
+++ inits/22-company.el
@@ -10,19 +10,19 @@
   :init
   ;; Yasnippet の補完を常に有効にする
   ;; https://github.com/syl20bnr/spacemacs/pull/179
-  ;; (defvar company-mode/enable-yas t
-  ;;   "Enable yasnippet for all backends.")
-  ;; (defun company-mode/backend-with-yas (backend)
-  ;;   (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-  ;;       backend
-  ;;     (append (if (consp backend) backend (list backend))
-  ;;             '(:with company-yasnippet))))
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+  (defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
 
   (add-hook 'after-change-major-mode-hook
             (lambda ()
               ;; メジャーモード切り替え時に，ヤスニペット補完のための backend 追加
-              ;; (if (eq company-mode t)
-              ;;     (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
+              (if (eq company-mode t)
+                  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
             ))
 
   :config
---
+++ inits/30-edit-mode-vue.el
@@ -0,0 +1,8 @@
+;;; 30-edit-mode-vue.el --- Edit mode for vue
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package vue-mode
+  :mode (("\\.vue\\'" . vue-mode))
